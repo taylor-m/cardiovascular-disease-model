7b9febc17607e68b17b0692030fa46c0b8c9f30f
taylor-m <53980659+taylor-m@users.noreply.github.com>
2021-01-05 02:16:19 -0600

    decreased train test split size for reduced processing time

--- cardio_app.py
+++ cardio_app.py
@@ -110,7 +110,7 @@ def main():
         y = df.disease
 
         X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=0.1, random_state=28, stratify=df.disease
+            X, y, test_size=0.2, random_state=28, stratify=df.disease
         )
         return X_train, X_test, y_train, y_test
 
@@ -134,7 +134,7 @@ def main():
         y = df.disease
 
         X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=0.1, random_state=28, stratify=df.gender
+            X, y, test_size=0.2, random_state=28, stratify=df.gender
             # stratification of the data on gender increases the predictive accuracy of the logistic regression model
             # because the data is unbalanced towards women; ~2/3 women 1/3 men
         )
@@ -319,7 +319,7 @@ def main():
         }
         # fmt: on
         # pipeline grid search cv fit
-        lr_pipeline_cv = GridSearchCV(lr_pipeline, lr_grid, cv=5, verbose=1, n_jobs=2)
+        lr_pipeline_cv = GridSearchCV(lr_pipeline, lr_grid, cv=2, verbose=1, n_jobs=2)
         lr_pipeline_cv.fit(X_train, y_train)
 
         # best hyperparameters
